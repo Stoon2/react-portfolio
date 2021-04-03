fe7f3091324d976d724685c8badd0ce3a43f73c3
Stoon <melsayed.se@gmail.com>
2021-04-03 19:01:49 +0200

    Added styling to AboutSection

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,28 +1,62 @@
 import home1 from '../img/home1.png';
+// Styles
+import styled from 'styled-components';
 
 const AboutSection = () => {
     return (
-        <div>
-            <div className="description">
+        <StyledAbout>
+            <SyledDescription>
                 <div className="title">
-                    <div className="hide">
+                    <StyledHide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide>
                         <h2>your <span>dreams</span> come</h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide>
                         <h2>true</h2>
-                    </div>
+                    </StyledHide>
                 </div>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima praesentium modi nulla error? Adipisci odio iure qui inventore hic vitae eveniet a ut repellendus aperiam, autem incidunt, non quaerat quasi!</p>
                 <button>Contact Us</button>
-            </div>
-            <div className="image">
+            </SyledDescription>
+            <StyledImage>
                 <img src={home1} alt="guy with camera"/>
-            </div>
-        </div>
+            </StyledImage>
+        </StyledAbout>
     )
-}
+};
+
+// Styled Component
+const StyledAbout = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+
+const SyledDescription = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+    h2 {
+        font-weight: lighter;
+    }
+`;
+
+const StyledImage = styled.div`
+    flex: 1;
+    overflow: hidden;
+    img {
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`;
+
+const StyledHide = styled.div`
+    overflow: hidden;
+`;
 
 export default AboutSection;
