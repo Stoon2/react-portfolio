e329c659071350d40ec61aaa9bab74dabe33aa86
Stoon <melsayed.se@gmail.com>
2021-04-24 00:10:52 +0200

    Styled About Section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,27 +1,74 @@
-
+import styled from "styled-components";
+import {StyledAbout} from "../styles";
 
 const FaqSection = () => {
     return (
-        <div className="faq">
+        <StyledFaq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start?</h4>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et quod, sit voluptatibus vitae debitis voluptates illum quaerat libero laudantium vel ut ab? Nemo delectus libero eligendi, accusamus architecto alias? Placeat?</p>                
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et quod, sit voluptatibus vitae debitis voluptates illum quaerat libero laudantium vel ut ab? Nemo delectus libero eligendi, accusamus architecto alias? Placeat?</p>                
+                </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily Schedule</h4>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et quod, sit voluptatibus vitae debitis voluptates illum quaerat libero laudantium vel ut ab? Nemo delectus libero eligendi, accusamus architecto alias? Placeat?</p>                
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et quod, sit voluptatibus vitae debitis voluptates illum quaerat libero laudantium vel ut ab? Nemo delectus libero eligendi, accusamus architecto alias? Placeat?</p>                
+                </div>
+                <div className="faq-line"></div>                
             </div>
             <div className="question">
                 <h4>Different Payment Methods</h4>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et quod, sit voluptatibus vitae debitis voluptates illum quaerat libero laudantium vel ut ab? Nemo delectus libero eligendi, accusamus architecto alias? Placeat?</p>                
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et quod, sit voluptatibus vitae debitis voluptates illum quaerat libero laudantium vel ut ab? Nemo delectus libero eligendi, accusamus architecto alias? Placeat?</p>                
+                </div>
+                <div className="faq-line"></div>               
             </div>
             <div className="question">
                 <h4>What products do you offer?</h4>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et quod, sit voluptatibus vitae debitis voluptates illum quaerat libero laudantium vel ut ab? Nemo delectus libero eligendi, accusamus architecto alias? Placeat?</p>                
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et quod, sit voluptatibus vitae debitis voluptates illum quaerat libero laudantium vel ut ab? Nemo delectus libero eligendi, accusamus architecto alias? Placeat?</p>                
+                </div>
+                <div className="faq-line"></div>               
             </div>
-        </div>
+        </StyledFaq>
     )
 }
+
+const StyledFaq = styled(StyledAbout)`
+    display: block;
+    span {
+        display: block
+    }
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+
+    .faq-line{
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+
+    .answer{
+        padding: 2rem 0rem;
+        p {
+            padding: 1rem 0rem;
+        }
+    }
+`;
 
 export default FaqSection;
