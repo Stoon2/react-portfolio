6b2163c759d24910f7d4766062c7a8269e0445e7
Stoon <melsayed.se@gmail.com>
2021-04-23 23:55:29 +0200

    Styled the Services Section, added styles.js for readability

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,11 +1,12 @@
 import home1 from '../img/home1.png';
 // Styles
-import styled from 'styled-components';
+// import styled from 'styled-components';
+import {StyledAbout, StyledDescription, StyledImage, StyledHide} from "../styles";
 
 const AboutSection = () => {
     return (
         <StyledAbout>
-            <SyledDescription>
+            <StyledDescription>
                 <div className="title">
                     <StyledHide>
                         <h2>We work to make</h2>
@@ -19,44 +20,12 @@ const AboutSection = () => {
                 </div>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima praesentium modi nulla error? Adipisci odio iure qui inventore hic vitae eveniet a ut repellendus aperiam, autem incidunt, non quaerat quasi!</p>
                 <button>Contact Us</button>
-            </SyledDescription>
+            </StyledDescription>
             <StyledImage>
                 <img src={home1} alt="guy with camera"/>
             </StyledImage>
         </StyledAbout>
     )
 };
-
-// Styled Component
-const StyledAbout = styled.div`
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: white;
-`;
-
-const SyledDescription = styled.div`
-    flex: 1;
-    padding-right: 5rem;
-    h2 {
-        font-weight: lighter;
-    }
-`;
-
-const StyledImage = styled.div`
-    flex: 1;
-    overflow: hidden;
-    img {
-        width: 100%;
-        height: 80vh;
-        object-fit: cover;
-    }
-`;
-
-const StyledHide = styled.div`
-    overflow: hidden;
-`;
 
 export default AboutSection;
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -9,12 +9,14 @@ const GlobalStyle = createGlobalStyle`
 
     body{
         background: #1b1b1b;
+        font-family: 'Inter', sans-serif;
     }
 
     button {
         cursor: pointer;
         font-weight: bold;
         font-size: 1.1rem;
+        font-family: 'Inter', sans-serif;
         padding: 1rem 2rem;
         border: 3px solid #23d997;
         background: transparent;
